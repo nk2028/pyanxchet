695a53782ff01a0e10125cfa49b4417460ec9178
Ayaka Mikazuki <ayaka@mail.shn.hk>
2021-01-22 23:50:36 +0800

    Update algorithm

--- src/autoderiver/index.js
+++ src/autoderiver/index.js
@@ -1,4 +1,5 @@
 import kyonh1 from './kyonh.js';
+import putonghua1 from './putonghua.js';
 import gwongzau1 from './gwongzau.js';
 
 function noThrow(f) {
@@ -12,9 +13,10 @@ function noThrow(f) {
 }
 
 const kyonh = noThrow(kyonh1);
+const putonghua = noThrow(putonghua1);
 const gwongzau = noThrow(gwongzau1);
 
 export default function 生成擬音(音韻地位) {
-  return `（古韻羅馬字: ${kyonh(音韻地位)}，推導廣州音: ${gwongzau(音韻地位)}）`; // TODO: Fix autoderiver
+  return `（古韻羅馬字: ${kyonh(音韻地位)}，推導普通話: ${putonghua(音韻地位)}，推導廣州音: ${gwongzau(音韻地位)}）`; // TODO: Fix autoderiver
   // return `推導日語漢音: ${kanon(音韻地位)}`;
 }
